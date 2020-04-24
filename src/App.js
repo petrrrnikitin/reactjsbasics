7fcc587516fb898633c1fee3e2713b6363e4316a
Petr Nikitin <nikitablvck@gmai.com>
2020-04-25 00:11:37 +0300

    Основы React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Car from "./Car/Car";
 
-function App() {
-  const divStyle = {
-    textAlign: 'left',
-    paddingLeft: '910px',
-    paddingTop: '100px'
-  }
-  return (
-    <div style={divStyle} className="App">
-    <h1 style={{ fontSize: '50px', color: 'red'}}>Hello Sonya!</h1>
-      <Car name='Ferrari 599' year={2020}><p style={{color: 'red'}}>COLOR</p></Car>
-      <Car name='BMW M3 COUPE' year={2005}><p style={{color: 'black'}}>COLOR</p></Car>
-      <Car name='TOYOTA GT 86' year={2015}><p style={{color: 'yellow'}}>COLOR</p></Car>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        cars: [
+            {name: 'Ferrari 599', year: 2020},
+            {name: 'BMW M3 COUPE', year: 2005},
+            {name: 'TOYOTA GT 86', year: 2015},
+        ],
+        pageTitle: 'React components',
+        showCars: false
+    };
+
+    onChangeName(name, index) {
+        const car = this.state.cars[index]
+        car.name = name
+        const cars = [...this.state.cars]
+        cars[index] = car
+        this.setState({cars})
+    }
+    handleInput = (event) => {
+        this.setState({pageTitle: event.target.value})
+    }
+    deleteHandler(index) {
+        const cars = this.state.cars.concat()
+        cars.splice(index, 1)
+        this.setState({cars})
+    }
+    toggleCarsHandler = () => {
+        this.setState( {
+            showCars: !this.state.showCars
+        })
+    }
+
+
+    render() {
+        const divStyle = {
+            paddingTop: '100px'
+        };
+
+        let cars = null
+
+        if (this.state.showCars) {
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car
+                        key={index}
+                        name={car.name}
+                        year={car.year}
+                        onDelete={this.deleteHandler.bind(this, index)}
+                        onChangeName={event => this.onChangeName(event.target.value, index)}
+                    />
+                )
+            })
+        }
+
+        return (
+            <div style={divStyle} className="App">
+                <h1 style={{fontSize: '50px', color: 'red'}}>{this.state.pageTitle}</h1>
+                <input onChange={this.handleInput} value={this.state.pageTitle} type="text"/>
+                <button onClick={this.toggleCarsHandler}>Toggle cars</button>
+                {cars}
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default props => (
-    <div>
+    <div style={{border: '1px solid #ccc',marginBottom: '10px', display: 'block', padding: '10px'}}>
         <h2>{props.name}</h2>
         <p>Year: {props.year}</p>
-        {props.children}
+        <input onChange={props.onChangeName} value={props.name} type="text"/>
+        <button onClick={props.onDelete}>Delete</button>
     </div>
 )
